a4bee94deb77e7ead8a1caf63e814c260a84b0dc
sadegh jafari <sadeghji1998@gmail.com>
2024-11-08 01:09:25 +0330

    Add project files.

---
+++ BackEnd_Trainee_Task_Git_GitHub/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEnd_Trainee_Task_Git_GitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}

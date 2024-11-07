96384942a2966836b7f2b778e9fcfa90ecea4d4d
Sadegh Jafari <sadeghji1998@gmail.com>
2024-11-08 01:29:15 +0330

    The first Commit

---
+++ BackEnd_Trainee_Task_Git_GitHub/Categories.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEnd_Trainee_Task_Git_GitHub
+{
+     class Categories
+    {
+
+        public static int Category1 { get; set; } = 1;
+        public static int Category2 { get; set; } = 2;
+        public static int Category3 { get; set; } = 3;
+
+        public Categories() {}
+
+        public override string ToString()
+        {
+            return String.Format("Category1: {0}\nCategory2: {1}\nCategory3: {2}",Category1,Category2,Category3);
+        }
+    }
+}
---
+++ BackEnd_Trainee_Task_Git_GitHub/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEnd_Trainee_Task_Git_GitHub
+{
+     class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Category { get; set; }
+        public int Price { get; set; }
+
+        public Product() {}
+
+        public Product(int Id, string Name, int Category, int Price)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Category = Category;
+            this.Price = Price;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("ID: {0}\nName: {1}\nCategory: {2}\nPrice: {3}",Id,Name,Category,Price);
+        }
+
+
+    }
+}

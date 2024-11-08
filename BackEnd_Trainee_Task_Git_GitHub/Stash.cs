f90b63bf237140887c8754aa16297ec1a2bce3ef
Sadegh Jafari <sadeghji1998@gmail.com>
2024-11-08 05:07:45 +0330

    stash

---
+++ BackEnd_Trainee_Task_Git_GitHub/Stash.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEnd_Trainee_Task_Git_GitHub
+{
+    internal class Stash
+    {
+        public int MyProperty { get; set; }
+
+    }
+}
